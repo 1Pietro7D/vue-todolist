51835656bb997a013eba6b95dfb23d81882dc8ef
PietroDanieli <1pie7d@gmail.com>
2022-09-15 06:28:39 +0200

    delete function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 const app = new Vue({
   el: "#app",
-  data: {},
-  methods: {},
+  data: {
+    todos: [
+      {
+        article: "pasta",
+        justDo: true,
+      },
+      {
+        article: "cheese",
+        justDo: false,
+      },
+      {
+        article: "bread",
+        justDo: false,
+      },
+      {
+        article: "meat",
+        justDo: false,
+      },
+    ],
+    archive: [],
+  },
+  methods: {
+    deleteItem(index) {
+      console.log(this.todos);
+      this.todos.splice(index, 1);
+      // COSE STRANE DA QUI IN POI
+      // this.archive.push(this.todos[index].article);
+      // console.log(this.archive);
+    },
+  },
 });
