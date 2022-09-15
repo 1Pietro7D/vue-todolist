c69d6669136950c4b5422555842c3cea03859508
PietroDanieli <1pie7d@gmail.com>
2022-09-15 09:34:45 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -19,15 +19,25 @@ const app = new Vue({
         justDo: false,
       },
     ],
+    something: "",
+    newTodo: {
+      article: "",
+      justDo: true,
+    },
     archive: [],
   },
   methods: {
+    addItem() {
+      console.log("miao");
+      this.newTodo.article = this.something;
+      this.todos.push(this.newTodo);
+    },
     deleteItem(index) {
       console.log(this.todos);
       this.todos.splice(index, 1);
       // COSE STRANE DA QUI IN POI
-      // this.archive.push(this.todos[index].article);
-      // console.log(this.archive);
+      this.archive.push(this.todos[index].article);
+      console.log(this.archive);
     },
   },
 });
